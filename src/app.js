2d0623a61bb9e6d8dcef9ff3827933b11b41b365
praie <85028563+praie@users.noreply.github.com>
2024-05-10 00:58:15 +0300

    Added search functionality using Api

--- src/app.js
+++ src/app.js
@@ -1,9 +1,26 @@
-function search(event) {
-  event.preventDefault();
-  let searchInputElement = document.querySelector(".Search-form-input");
-  let cityElement = document.querySelector(".city");
-  cityElement.innerHTML = searchInputElement.value;
+function updateWeather(response) {
+  let temperatureElement = document.querySelector("#current-temperature");
+  let temperature = response.data.temperature.current;
+  let cityElement = document.querySelector("#current-city");
+
+  cityElement.innerHTML = response.data.city;
+  temperatureElement.innerHTML = Math.round(temperature);
 }
 
-let searchForm = document.querySelector(".search-form");
-searchForm.addEventListener("submit", search);
+function searchCity(city) {
+  let apiKey = "4t0029f88f229dc5e5o10e3ba24fdd6a";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+  axios.get(apiUrl).then(updateWeather);
+}
+
+function search(event) {
+  event.preventDefault();
+  let searchInput = document.querySelector("#search-form-input");
+
+  searchCity(searchInput.value);
+}
+
+let searchFormElement = document.querySelector("#search-form");
+searchFormElement.addEventListener("submit", search);
+
+searchCity("Harare");
