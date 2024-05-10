da124481ee28b1592e8557a102b9ff7fbb2a2a10
praie <85028563+praie@users.noreply.github.com>
2024-05-10 18:39:17 +0300

    injected all weather data

--- src/app.js
+++ src/app.js
@@ -1,10 +1,47 @@
 function updateWeather(response) {
   let temperatureElement = document.querySelector("#current-temperature");
+  let icon = document.querySelector("#icon");
   let temperature = response.data.temperature.current;
   let cityElement = document.querySelector("#current-city");
+  let timeElement = document.querySelector("#time");
+  let date = new Date(response.data.time * 1000);
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windSpeedElement = document.querySelector("#windSpeed");
 
   cityElement.innerHTML = response.data.city;
+  icon.innerHTML = `<img src="${response.data.condition.icon_url}" class="temperature-icon"/ >`;
   temperatureElement.innerHTML = Math.round(temperature);
+  timeElement.innerHTML = formatDate(date);
+  descriptionElement.innerHTML = response.data.condition.description;
+  humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
+  windSpeedElement.innerHTML = `${response.data.wind.speed}km/h`;
+}
+function formatDate(date) {
+  let minutes = date.getMinutes();
+  let hours = date.getHours();
+  let day = date.getDay();
+
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+
+  let formattedDay = days[day];
+  return `${formattedDay} ${hours}:${minutes}`;
 }
 
 function searchCity(city) {
