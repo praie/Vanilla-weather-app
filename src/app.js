aef20092193969868d7807f722dbbdc09427daff
praie <85028563+praie@users.noreply.github.com>
2024-05-25 19:22:34 +0300

    injected forecast details from javascript

--- src/app.js
+++ src/app.js
@@ -56,8 +56,35 @@ function search(event) {
 
   searchCity(searchInput.value);
 }
+function displayForecast() {
+  let days = ["Mon", "Tue", "Wed", "Thur", "Fri", "Sat", "Sun"];
+  let forecastHtml = "";
+
+  days.forEach(function (day) {
+    forecastHtml =
+      forecastHtml +
+      `
+      
+      <div class="weather-forecast-day">
+      <div class="weather-forecast-date">${day}</div>
+      <div class="weather-forecast-icon">☀️</div>
+      <div class="weather-forecast-temperatures">
+      <div class="weather-forecast-temperature">
+      <strong>15°</strong>
+      </div>
+      <div class="weather-forecast-temperature"> 9°</div>
+      </div>
+      </div>
+
+      `;
+  });
+
+  let forecastElement = document.querySelector("#forecast");
+  forecastElement.innerHTML = forecastHtml;
+}
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", search);
 
 searchCity("Harare");
+displayForecast();
